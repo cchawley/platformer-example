10727a50d8a1b5d00637ad3107e05881144d0abf
connor hawley <rcohawley@yahoo.com>
2020-03-19 15:54:51 -0500

    Finially got the tileset and tilemap to build correctly

---
+++ PlatformerExample/Content/BetterSet2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="BetterSet2" tilewidth="21" tileheight="21" spacing="2" margin="3" tilecount="900" columns="30">
+ <image source="spritesheet.png" trans="5e81a2" width="692" height="692"/>
+</tileset>
--- PlatformerExample/Content/tiledspritesheet.tsx
+++ PlatformerExample/Content/tiledspritesheet.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="spritesheet" tilewidth="21" tileheight="21" spacing="2" margin="2" tilecount="480" columns="30">
+<tileset version="1.2" tiledversion="1.3.3" name="spritesheet" tilewidth="21" tileheight="21" spacing="2" margin="2" tilecount="480" columns="30">
  <image source="spritesheet.png" trans="5e81a2" width="694" height="372"/>
  <tile id="0">
   <properties>
